b5e3b1ffe6316352ad3a7e7a1de52c24786f6b65
sangwon-lee302 <200340413+sangwon-lee302@users.noreply.github.com>
2026-03-30 17:43:04 +0900

    build: setup development environment
    Setup Laravel Sail, install Fortify and configure dev environment.
    
    - Install Laravel Sail.
    - Install Laravel Fortify.
    - Setup Mailpit.
    - Setup prettier and its plugins for blade and tailwindcss.
    - Configure Laravel Pint.
    - Setup Larastan.
    - Setup husky and lint-staged.
    - Setup Laravel IDE Helper and add "ide" command to composer.json for
    updating helper files.
    - Install Laravel Debugbar.
    - Pin the version of node with Volta.
    - Fix version info in README.md.

---
+++ README.md
@@ -0,0 +1,27 @@
+# COACHTECHフリマ
+
+## 概要
+
+プログラミングの学習を目的としたフリマアプリを作成するプロジェクト。ユーザー認証、マイページ、商品の閲覧、出品、購入などの基本的な機能を有する。
+
+## 環境構築の手順
+
+- `git clone git@github.com:sangwon-lee302/flee-market.git`
+- `cd flee-market`
+- `docker run --rm -u "$(id -u):$(id -g)" -v "$(pwd)":/opt -w /opt laravelsail/php84-composer:latest composer install --ignore-platform-reqs`
+- `cp .env.example .env`, `.env`編集(OS環境変数を適宜変更)
+- `sail up -d`
+- `sail artisan key:generate`
+- `sail artisan migrate --seed`
+- `sail npm i && sail npm run dev`
+
+## 使用技術
+
+- Laravel 13.2.0
+- PHP 8.5.3
+- Mysql 8.4.8
+- Node 24.14.0
+
+## ER図
+
+![ER図](./database-design.drawio.svg)
---
+++ app/Models/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
+use Illuminate\Notifications\Notifiable;
+
+/**
+ * @property-read DatabaseNotificationCollection<int, DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ *
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User query()
+ *
+ * @mixin \Eloquent
+ */
+#[Fillable(['name', 'email', 'password'])]
+#[Hidden(['password', 'remember_token'])]
+class User extends Authenticatable
+{
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
