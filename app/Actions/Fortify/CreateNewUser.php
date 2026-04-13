dc6ae5b19eef4894fc08f876c469f1255a5c5071
sangwon-lee302 <200340413+sangwon-lee302@users.noreply.github.com>
2026-04-13 23:41:55 +0900

    feat: implement transaction for user registration
    - Implement transaction that tries to create a new user and its empty
    profile.

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -3,7 +3,10 @@
 namespace App\Actions\Fortify;
 
 use App\Http\Requests\RegisterRequest;
+use App\Models\Profile;
 use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
@@ -11,8 +14,6 @@ use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class CreateNewUser implements CreatesNewUsers
 {
-    use PasswordValidationRules;
-
     /**
      * Validate and create a newly registered user.
      *
@@ -22,15 +23,24 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
-        Validator::make(
-            $input,
-            (new RegisterRequest)->rules()
-        )->validate();
+        Validator::make($input, (new RegisterRequest)->rules())->validate();
 
-        return User::create([
-            'name'     => $input['name'],
-            'email'    => $input['email'],
-            'password' => Hash::make($input['password']),
-        ]);
+        return DB::transaction(function () use ($input) {
+            try {
+                $user = User::create([
+                    'name'     => $input['name'],
+                    'email'    => $input['email'],
+                    'password' => Hash::make($input['password']),
+                ]);
+
+                $user->profile()->save(new Profile);
+
+                return $user;
+            } catch (Exception $e) {
+                throw ValidationException::withMessages([
+                    'email' => '登録処理に失敗しました。時間をおいて再度お試しください。',
+                ]);
+            }
+        });
     }
 }
