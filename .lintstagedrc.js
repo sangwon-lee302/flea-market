05b58979f1f9cc6f23b003e170b03081da438f89
sangwon-lee302 <200340413+sangwon-lee302@users.noreply.github.com>
2026-04-17 16:07:46 +0900

    refactor: remove "semi": false from .prettierrc
    - Modify .prettierrc.
    - Run formatting with the renewed configuration.

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,12 @@
-import path from 'node:path'
+import path from 'node:path';
 
 export default {
     '**/*.php': (filenames) => {
         const relativeFiles = filenames
             .map((file) => path.relative(process.cwd(), file))
-            .join(' ')
+            .join(' ');
 
-        return `./vendor/bin/sail bin pint ${relativeFiles}`
+        return `./vendor/bin/sail bin pint ${relativeFiles}`;
     },
     '**/*.{blade.php,js,css,json,md}': ['npx prettier --write'],
-}
+};
