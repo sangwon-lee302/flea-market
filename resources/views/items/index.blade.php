7a80d7633e099d5db640cd88c39de0cbf273e61b
sangwon-lee302 <200340413+sangwon-lee302@users.noreply.github.com>
2026-04-20 10:58:30 +0900

    refactor: remove unnecessary comment in item index view

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,6 +1,5 @@
 <x-app-layout class="mt-8">
     <x-items-list-with-nav
-        {{-- better rewriting urls using official helpers --}}
         :items="$items"
         :links="[
             'おすすめ'   => ['name' => 'items.index', 'exclude' => ['tab']],
