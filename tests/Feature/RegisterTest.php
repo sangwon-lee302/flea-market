a7f2dd930dea9c947b5b0e813bfcced742fbfc6a
sangwon-lee302 <200340413+sangwon-lee302@users.noreply.github.com>
2026-04-22 21:55:48 +0900

    feat: implement test_user_cannot_register_with_empty_email

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -9,10 +9,7 @@ class RegisterTest extends TestCase
 {
     use RefreshDatabase;
 
-    /**
-     * A basic feature test example.
-     */
-    public function test_user_cannot_register_with_invalid_name(): void
+    public function test_user_cannot_register_with_empty_name(): void
     {
         $response = $this->get(route('register'));
         $response->assertOk();
@@ -26,5 +23,21 @@ class RegisterTest extends TestCase
 
         $response->assertRedirect(route('register'));
         $response->assertInvalid(['name' => 'お名前を入力してください']);
+    }
+
+    public function test_user_cannot_register_with_empty_email(): void
+    {
+        $response = $this->get(route('register'));
+        $response->assertOk();
+
+        $response = $this->post(route('register.store'), [
+            'name'                  => 'taro yamada',
+            'email'                 => '',
+            'password'              => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertRedirect(route('register'));
+        $response->assertInvalid(['email' => 'メールアドレスを入力してください']);
     }
 }
